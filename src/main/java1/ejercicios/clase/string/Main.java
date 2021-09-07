3b12b40663121dd6d5f9f47e27c079a525f48f81
Nathan Velez <veleznathan64@gmail.com>
2021-09-06 20:10:48 -0500

    Taller en clase

---
+++ src/main/java1/ejercicios/clase/string/Main.java
@@ -0,0 +1,41 @@
+package ejercicios.clase.string;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+
+		// 1 contains
+		// String clase utilitaria para manejo de cadenas
+		String frase = "Programacion de algoritmos modulo II";
+
+		System.out.println("Ingrese el nombre del libro");
+		String palabra = teclado.nextLine();
+
+		boolean resultado = frase.contains(palabra);
+		System.out.println("Contiene la plabra: " + resultado);
+
+		// 2 equals
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		String cadena1 = "nathan";
+		System.out.println("Ingrese la segunda cadena a comparar");
+		String cadena2 = teclado.nextLine();
+		boolean resultado2 = cadena1.equals(cadena2);
+		System.out.println("Las cadenas son iguales: " + resultado2);
+
+		// 3 y 4 Upper Lower
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++");
+		String nombre = "juanito";
+		String nombreEnMayuscula = nombre.toUpperCase();
+		System.out.println("Nombre en mayuscula: " + nombreEnMayuscula);
+
+		String nombre2 = "JOSE";
+		String nombreEnMinuscula = nombre2.toLowerCase();
+		System.out.println("Nombre en minuscula: " + nombreEnMinuscula);
+
+	}
+
+}
---
+++ src/main/java1/ejercicios/variables/locales/globales/Paciente.java
@@ -0,0 +1,20 @@
+package ejercicios.variables.locales.globales;
+
+public class Paciente {
+	
+	private String nombre;
+	private int edad;
+	
+	public void deporte() {
+		this.nombre="Paul";
+		this.edad=25;
+		String actividad="Futbol";
+		int codigoDeporte=001;
+		actividad="Basquet";
+	}
+	
+	
+	
+	
+
+}
