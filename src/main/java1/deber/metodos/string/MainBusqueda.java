2b0cb641417e53d20655598e7f9ac5faf7cdfe7c
Nathan Velez <veleznathan64@gmail.com>
2021-09-23 22:39:54 -0500

    taller coregido

--- src/main/java1/deber/metodos/string/MainBusqueda.java
+++ src/main/java1/deber/metodos/string/MainBusqueda.java
@@ -10,87 +10,39 @@ public class MainBusqueda extends Estudiante {
 
 		Scanner teclado = new Scanner(System.in);
 		Scanner teclado2 = new Scanner(System.in);
-
-		System.out.println("*************************************");
-		System.out.println("");
-		System.out.println("       1. Ingresar estudiante");
-		System.out.println("       2. Buscar estudiante");
-		System.out.println("       3. SALIR");
-		System.out.println("");
-		System.out.println("*************************************");
-
-		int num = teclado.nextInt();
-		int numEstudiante = -1;
-
-		Estudiante[] datosCedula = new Estudiante[5];
-
-		while (num == 1) {
-			System.out.println("Ingresar cédula del estudiante");
-			String cedula = teclado2.nextLine();
-			numEstudiante = numEstudiante + 1;
-
-			Estudiante cedulaEstudiante = new Estudiante();
-			cedulaEstudiante.setCedula(cedula);
-
-			datosCedula[numEstudiante] = cedulaEstudiante;
-
-			System.out.println("*************************************");
-			System.out.println("");
-			System.out.println("       1. Ingresar estudiante");
-			System.out.println("       2. Buscar estudiante");
-			System.out.println("       3. SALIR");
-			System.out.println("");
-			System.out.println("*************************************");
-
-			num = teclado.nextInt();
-
-			while (num == 2) {
-				System.out.println("Buscar cedula de estudiante");
-				String cedula2 = teclado2.nextLine();
-
-				boolean comparacion = false;
-
-				for (int i = 0; i < 5; i++) {
-					Estudiante cedulaComparar = datosCedula[i];
-					String cedula1 = cedulaComparar.getCedula();
-					boolean comparacionLocal = cedula1.equals(cedula2);
-
-					if (comparacionLocal == true) {
-						comparacion = true;
-					}
-
-				}
-				if (comparacion == true) {
+		int opcion;
+		int i = -1;
+		String cedulaIngresada = "";
+		Estudiante vector[]=new Estudiante[5]; 
+		do {
+			System.out.println("Elije una opcion");
+			System.out.println("1.Ingresar Estudiante");
+			System.out.println("2.Buscar Estudiante");
+			System.out.println("3.Salir");
+			opcion=teclado.nextInt();
+			if(opcion==1) {
+				System.out.println("Ingrese cedula del estudiante");
+				cedulaIngresada=teclado2.nextLine();
+				Estudiante cedulaEstudiante=new Estudiante();
+				cedulaEstudiante.setCedula(cedulaIngresada);
+				i=i+1;
+				vector[i]=cedulaEstudiante;
+				System.out.println("Se ha guardado la cedula");
+			}else if(opcion==2) {
+				System.out.println("Ingrese cedula a buscar");
+				String cedulaDeBusqueda=teclado2.nextLine();
+				if(cedulaIngresada==cedulaDeBusqueda) {
 					System.out.println("Estudiante registrado");
-					System.out.println("*************************************");
-					System.out.println("");
-					System.out.println("       1. Ingresar estudiante");
-					System.out.println("       2. Buscar estudiante");
-					System.out.println("       3. SALIR");
-					System.out.println("");
-					System.out.println("*************************************");
-
+					
 				} else {
-					System.out.println("Estudiante no registrado");
-					System.out.println("*************************************");
-					System.out.println("");
-					System.out.println("       1. Ingresar estudiante");
-					System.out.println("       2. Buscar estudiante");
-					System.out.println("       3. SALIR");
-					System.out.println("");
-					System.out.println("*************************************");
+					System.out.println("El estudiante no esta registrado");
 				}
-				num = teclado.nextInt();
-				if (num == 3) {
-					System.out.println("Saliendo del sistema");
-				}
+				
 			}
+
+			
+			
+		}while(opcion!=3);
+		System.out.println("Cerrando sistema");
 		}
-	}
-
-	@Override
-	public String toString() {
-		return "MainBusqueda [cedula=" + cedula + "]";
-	}
-
 }
